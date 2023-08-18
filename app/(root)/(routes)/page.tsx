838e36d2bb7e2591ba280b8bb30ffe50e6ae888b
MD. Taifur <Taifurislamashraf.bd@gmail.com>
2023-08-18 21:53:15 +0600

    done. create & update celebrity - and create new modal in prisma

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -3,17 +3,42 @@ import Protected from "@/components/Protected";
 import SearchInput from "@/components/SearchInput";
 import db from "@/lib/db";
 
+interface RootProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
 
-export default async function Home() {
-  
-  const cetegory = await db.cetegory.findMany()
+export default async function Home({ searchParams }: RootProps) {
+  console.log(searchParams);
+  const cetegory = await db.cetegory.findMany();
+
+  const data = await db.celebrity.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return (
     <Protected>
-   <div className="p-4 space-y-2 h-full">
-    <SearchInput />
-    <Cetegory data={cetegory} />
-   </div>
-   </Protected>
-  )
+      <div className="p-4 space-y-2 h-full">
+        <SearchInput />
+        <Cetegory data={cetegory} />
+      </div>
+    </Protected>
+  );
 }
