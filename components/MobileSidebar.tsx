b9e8553a4e67eeac1aa5893809ae22cc2e557958
MD. Taifur <Taifurislamashraf.bd@gmail.com>
2023-08-24 12:15:32 +0600

    some fix

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -9,7 +9,7 @@ const MobileSidebar = () => {
             <Menu />
         </SheetTrigger>
         <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-            <Sidebar />
+            <Sidebar isPro={false} />
         </SheetContent>
     </Sheet>
   )
